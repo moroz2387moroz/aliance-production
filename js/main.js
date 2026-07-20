771c40599d78cc8499766eddf6690a9c1f42b142
Marina Moroz <moroz2387moroz@yandex.ru>
2026-07-20 22:22:08 +1000

    fix: navbar's scroll

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-console.log("天官赐福");
+const navbar = document.querySelector(".navbar");
+
+window.addEventListener("scroll", () => {
+  if (this.scrollY > 1) {
+    navbar.classList.add("navbar-light");
+  } else {
+    navbar.classList.remove("navbar-light");
+  }
+});
