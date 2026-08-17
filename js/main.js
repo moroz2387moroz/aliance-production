e7276a1350945e639639b861ff7581174bab77e3
Marina Moroz <moroz2387moroz@yandex.ru>
2026-08-17 11:10:56 +1000

    add: phone's mask

--- js/main.js
+++ js/main.js
@@ -192,4 +192,46 @@ document.addEventListener("DOMContentLoaded", () => {
         ajaxSend(formData);
       });
   });
+
+  const phoneInputs = document.querySelectorAll(
+    'input[name="userphone"], input[type="tel"]',
+  );
+
+
+// маска для телефона
+  phoneInputs.forEach((input) => {
+    input.addEventListener("input", () => {
+      let value = input.value.replace(/\D/g, "").substring(0, 11);
+
+      if (!value) {
+        input.value = "";
+        return;
+      }
+
+      if (value.startsWith("8")) {
+        value = "7" + value.slice(1);
+      }
+
+      if (!value.startsWith("7")) {
+        value = "7" + value;
+      }
+
+      let formatted = "+7";
+
+      if (value.length > 1) {
+        formatted += " (" + value.slice(1, 4);
+      }
+      if (value.length > 4) {
+        formatted += ") " + value.slice(4, 7);
+      }
+      if (value.length > 7) {
+        formatted += "-" + value.slice(7, 9);
+      }
+      if (value.length > 9) {
+        formatted += "-" + value.slice(9, 11);
+      }
+
+      input.value = formatted;
+    });
+  });
 });
